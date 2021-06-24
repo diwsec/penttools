e928001377de0fd9874e9d8175a3deb1c721786b
diwakar mohanraj <83821864+diwusec@users.noreply.github.com>
2021-06-24 17:28:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,6 @@ sudo apt-get install -y jq
 sudo apt-get install -y ruby-full
 sudo apt-get install -y libpcap-dev
 sudo apt-get install -y htop
-sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev
-sudo apt-get install -y libssl-dev libffi-dev python-dev
 sudo apt-get install -y libldns-dev
 sudo apt-get install -y python-setuptools
 sudo apt-get install -y build-essential
