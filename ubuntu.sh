1f74dc38cc869e28e935491511002d05476eacde
diwakar mohanraj <83821864+diwusec@users.noreply.github.com>
2021-07-26 14:20:42 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -30,6 +30,7 @@ sudo apt-get install -y npm
 sudo apt-get install -y tree
 sudo apt-get install -y vim
 sudo apt-get install -y wireshark
+sudo apt-get install -y nmap
 sudo apt-get install -y masscan
 sudo apt-get install -y parallel
 sudo apt-get install -y jsbeautifier
