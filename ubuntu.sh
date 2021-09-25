4a38bc5c4bd768c3759be763418593ee41b929e4
diwakar <83821864+diwusec@users.noreply.github.com>
2021-09-25 18:05:51 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -96,7 +96,7 @@ cd ~/tools/
 echo "done"
 
 echo "installing favifreak"
-git clone https://github.com/diwusec/favifreak.git
+git clone https://github.com/devanshbatham/FavFreak.git
 cd ~/tools/
 echo "done"
 
@@ -126,7 +126,7 @@ cd ~/tools/
 echo "done"
 
 echo "installing corscanner"
-git clone https://github.com/diwusec/corscanner.git
+git clone https://github.com/chenjj/CORScanner.git
 cd ~/tools/
 echo "done"
 
