d5e1ac641771e0de86aeadd8600a84444eb57068
diwakar <83821864+diwsec@users.noreply.github.com>
2022-06-14 20:43:03 +0530

    rm vim,bashtop ppa

--- install.sh
+++ install.sh
@@ -18,8 +18,6 @@ sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 sudo add-apt-repository ppa:longsleep/golang-backports
 sudo add-apt-repository ppa:wireshark-dev/stable
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo add-apt-repository ppa:bashtop-monitor/bashtop
 sudo apt-get -y update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
